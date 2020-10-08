63364a327ba6a87f13f1312871408b9e6dce6267
Eric Rosenborg <eric.rosenborg@hiq.se>
2020-10-08 17:28:49 +0200

    Add function to handle changes in textArea and a function forhandling submit event

--- src/hooks/useForm.ts
+++ src/hooks/useForm.ts
@@ -1,20 +1,37 @@
+import { API, graphqlOperation } from "aws-amplify";
 import React, { useState, useEffect, useRef } from "react";
+import { createAdvertisement } from "../graphql/mutations";
 
 const useForm = (
         initialValues: {title: string, description: string}
     ) => {
 	const [values, setValues] = useState(initialValues || {});
 	
-	const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+	const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
 		const { target } = event;
-		const { title, value } = target;
+		const { value } = target;
 		event.persist();
 		setValues({ ...values, title: value });
-	  };
+	};
+
+	const handleTextAreaChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+		const { target } = event;
+		const { value } = target;
+		event.persist();
+		setValues({ ...values, description: value });
+		console.log(values.description)
+	};
+
+	const handleSubmit = async (event: any) => {
+		event.preventDefault()
+		await API.graphql(graphqlOperation(createAdvertisement, {input: values}))
+	}
 
 	  return {
 		  values,
-		  handleChange
+		  handleInputChange,
+		  handleTextAreaChange,
+		  handleSubmit
 	  }
 	  
 

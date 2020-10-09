ef51122c1997031040c1490bb37b21311711cab4
Rubi Kuo <rubitiara@gmail.com>
2020-10-09 10:46:34 +0200

    styled

--- src/pages/ItemDetails.tsx
+++ src/pages/ItemDetails.tsx
@@ -1,6 +1,22 @@
 /* eslint-disable react/button-has-type */
 import React, { FC } from "react";
 import { useParams } from "react-router-dom";
+import styled from "styled-components";
+
+const MainDiv = styled.div`
+  border: 1px solid black;
+  display: flex;
+  flex-direction: column;
+`;
+const DescList = styled.ul`
+  list-style: none;
+`;
+
+const ItemImg = styled.img`
+  width: 300px;
+  height: 300px;
+  margin: 0 auto;
+`;
 
 interface ParamTypes {
   id: string;
@@ -10,21 +26,24 @@ const ItemDetails: FC<ParamTypes> = () => {
   const { id } = useParams<ParamTypes>();
 
   return (
-    <main>
+    <MainDiv>
       <h1>Designed Table</h1>
-      <img src="" alt="" />
-      <ul>
+      <ItemImg
+        src="https://storage.googleapis.com/web-pro-nilo-kavehome/media/cache/c4/10/c410118add2b5cb169d71a0c20596f50.jpg"
+        alt=""
+      />
+      <DescList>
         <li>Item id:{id} </li>
         <li>Measurement: 20x10x10</li>
         <li>Material: Wood</li>
         <li>Brand: Ikea</li>
-      </ul>
+      </DescList>
       <article>Description</article>
       <div>
         <button>Back</button>
         <button>Reserve</button>
       </div>
-    </main>
+    </MainDiv>
   );
 };
 

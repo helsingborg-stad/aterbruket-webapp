2b086adfe3badb051ccc6aa8e40b2b1616f4dc25
Rubi Kuo <rubitiara@gmail.com>
2020-10-09 11:19:10 +0200

    save

--- src/pages/ItemDetails.tsx
+++ src/pages/ItemDetails.tsx
@@ -8,15 +8,28 @@ const MainDiv = styled.div`
   display: flex;
   flex-direction: column;
 `;
-const DescList = styled.ul`
-  list-style: none;
-`;
 
 const ItemImg = styled.img`
   width: 300px;
   height: 300px;
   margin: 0 auto;
 `;
+
+const DescTable = styled.table`
+  border: 1px solid purple;
+  width: 500px;
+  margin: 0 auto;
+`;
+
+const DescThead = styled.thead``;
+
+const DescTbody = styled.tbody``;
+
+const DescTh = styled.th``;
+
+const DescTd = styled.td``;
+
+const DescTr = styled.tr``;
 
 interface ParamTypes {
   id: string;
@@ -32,12 +45,26 @@ const ItemDetails: FC<ParamTypes> = () => {
         src="https://storage.googleapis.com/web-pro-nilo-kavehome/media/cache/c4/10/c410118add2b5cb169d71a0c20596f50.jpg"
         alt=""
       />
-      <DescList>
-        <li>Item id:{id} </li>
+      <DescTable>
+        <DescTbody>
+          <DescTr>
+            <DescTd>Item number:</DescTd>
+            <DescTd>{id}</DescTd>
+          </DescTr>
+          <DescTr>
+            <DescTd>Measurement</DescTd>
+            <DescTd>20 x 30 x 10 cm</DescTd>
+          </DescTr>
+          <DescTr>
+            <DescTd>Brand</DescTd>
+            <DescTd>IKEA</DescTd>
+          </DescTr>
+        </DescTbody>
+        {/* <li>Item id:{id} </li>
         <li>Measurement: 20x10x10</li>
         <li>Material: Wood</li>
-        <li>Brand: Ikea</li>
-      </DescList>
+        <li>Brand: Ikea</li> */}
+      </DescTable>
       <article>Description</article>
       <div>
         <button>Back</button>

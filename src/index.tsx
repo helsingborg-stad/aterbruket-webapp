e03d65285f8a1a3b8c7c826709428eba12355cba
Rubi Kuo <rubitiara@gmail.com>
2020-10-09 14:26:50 +0200

    initial table

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import Amplify from "aws-amplify";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import Amplify from "aws-amplify";
-//import awsExports from "./aws-exports";
+import awsExports from "./aws-exports";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -12,7 +12,7 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
-//Amplify.configure(awsExports);
+Amplify.configure(awsExports);
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA

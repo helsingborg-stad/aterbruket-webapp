ccc75cedafc4fb2b90433c073fa6aeb6cc6d8d0a
Rubi Kuo <rubitiara@gmail.com>
2020-10-09 09:55:08 +0200

    initial detailpage

--- src/Components/AdvertContainer.tsx
+++ src/Components/AdvertContainer.tsx
@@ -1,37 +1,32 @@
-import { GraphQLResult } from "./node_modules/@aws-amplify/api";
-import { API } from "./node_modules/aws-amplify";
-import React from "./node_modules/react";
+import { GraphQLResult } from "@aws-amplify/api";
+import { API } from "aws-amplify";
+import React, { FC, useEffect, useState } from "react";
 import { ListAdvertisementsQuery } from "../API";
 import { listAdvertisements } from "../graphql/queries";
+import Card from "./Card";
 
-class AdvertContainer extends React.Component<unknown, any> {
-  constructor(props: any) {
-    super(props);
-    this.state = {
-      items: [],
-    };
-  }
+const AdvertContainer = () => {
+  const [items, setItems] = useState([]) as any;
 
-  async componentDidMount(): Promise<void> {
+  const fetchItems = async () => {
     const result = (await API.graphql({
       query: listAdvertisements,
     })) as GraphQLResult<ListAdvertisementsQuery>;
     const advertItems = result.data?.listAdvertisements?.items;
 
-    this.setState({
-      items: advertItems,
-    });
-  }
+    setItems(advertItems);
+  };
 
-  render() {
-    return (
-      <ul>
-        {this.state.items.map((item: { title: string }) => (
-          <li>{item.title}</li>
-        ))}
-      </ul>
-    );
-  }
-}
+  useEffect(() => {
+    fetchItems();
+  });
 
+  return (
+    <ul>
+      {items.map((item: any) => (
+        <Card title={item.title} description={item.description} />
+      ))}
+    </ul>
+  );
+};
 export default AdvertContainer;
--- src/pages/ItemDetails.tsx
+++ src/pages/ItemDetails.tsx
@@ -1,5 +1,6 @@
-import React, { FC } from 'react';
-import { useParams } from 'react-router-dom';
+/* eslint-disable react/button-has-type */
+import React, { FC } from "react";
+import { useParams } from "react-router-dom";
 
 interface ParamTypes {
   id: string;
@@ -8,7 +9,23 @@ interface ParamTypes {
 const ItemDetails: FC<ParamTypes> = () => {
   const { id } = useParams<ParamTypes>();
 
-  return <h1>Details about item with id: {id}</h1>;
+  return (
+    <main>
+      <h1>Designed Table</h1>
+      <img src="" alt="" />
+      <ul>
+        <li>Item id:{id} </li>
+        <li>Measurement: 20x10x10</li>
+        <li>Material: Wood</li>
+        <li>Brand: Ikea</li>
+      </ul>
+      <article>Description</article>
+      <div>
+        <button>Back</button>
+        <button>Reserve</button>
+      </div>
+    </main>
+  );
 };
 
 export default ItemDetails;

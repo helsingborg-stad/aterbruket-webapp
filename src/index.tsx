faed02d98fc102796dc336e23a4497fb27b3df31
Anna Eklöv <annaeklov90@gmail.com>
2020-10-12 11:10:49 +0200

    Add sandbox

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import Amplify from "aws-amplify";
-// import awsExports from "./aws-exports";
+import awsExports from "./aws-exports";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -12,7 +12,7 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
-// Amplify.configure(awsExports);
+Amplify.configure(awsExports);
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA

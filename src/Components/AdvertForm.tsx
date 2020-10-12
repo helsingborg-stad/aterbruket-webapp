b2e2c3d7443a392d42684688921fa1abb7173c24
Eric Rosenborg <eric.rosenborg@hiq.se>
2020-10-12 14:49:13 +0200

    Rewrite of AdvertForm component to be more generic

--- src/Components/AdvertForm.tsx
+++ src/Components/AdvertForm.tsx
@@ -1,35 +1,41 @@
 import React from "react";
 import useForm from "../hooks/useForm";
+import { CreateAdvertisementMutation } from "../API";
 
-export default function AdvertForm() {
-  const { values, handleInputChange, handleSubmit } = useForm({
-    title: "",
-    description: "",
-    height: 0,
-    width: 0,
-    length: 0,
+interface IFields {
+  name: string;
+  dataType?: string;
+  fieldType: any;
+}
+
+export default function AdvertForm(props: { fields: IFields[] }) {
+  const { values, handleInputChange, handleSubmit } = useForm({});
+
+  const fields = props.fields.map((field) => {
+    if (field.fieldType === "input") {
+      return (
+        <input
+          type={field.dataType}
+          name={field.name}
+          onChange={handleInputChange}
+          value={values[field.name]}
+        ></input>
+      );
+    } else if (field.fieldType === "textarea") {
+      return (
+        <textarea
+          name={field.name}
+          onChange={handleInputChange}
+          value={values[field.name]}
+        ></textarea>
+      );
+    }
   });
 
   return (
     <div>
       <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          name="title"
-          value={values.title}
-          onChange={handleInputChange}
-        ></input>
-        <input
-          type="text"
-          name="width"
-          value={values.width}
-          onChange={handleInputChange}
-        ></input>
-        <textarea
-          value={values.description}
-          onChange={handleInputChange}
-          name="description"
-        ></textarea>
+        {fields}
         <button type="submit">Submit</button>
       </form>
     </div>

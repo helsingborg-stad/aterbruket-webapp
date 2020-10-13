a365972c2b11b6cd004c4b17e5b0350f1b1ce52e
Rubi Kuo <rubitiara@gmail.com>
2020-10-13 16:31:02 +0200

    intial qr code

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,8 @@ import Home from "./pages/Home";
 import Header from "./components/Header";
 import AddItem from "./pages/AddItem";
 import ItemDetails from "./pages/ItemDetails";
-import AdvertContainer from "./components/AdvertContainer"
-import AdvertForm from "./components/Form"
+import AdvertContainer from "./components/AdvertContainer";
+import AdvertForm from "./components/Form";
 import { createAdvertisement } from "./graphql/mutations";
 
 const AppContainer = styled.div`
@@ -35,23 +35,14 @@ const AppContainer = styled.div`
 const App: FC = () => {
   const [modalOpen, setModalOpen] = useState(false);
 
-  const fields  = [
+  const fields = [
     {
-      name: 'title',
-      dataType: 'text',
-      fieldType: 'input'
+      name: "qrValue",
+      dataType: "text",
+      fieldType: "input",
     },
-    {
-      name: 'width',
-      dataType: 'number',
-      fieldType: 'input'
-    },
-    {
-      name: 'description',
-      fieldType: 'textarea'
-    }
-  ]
-  
+  ];
+
   return (
     <ThemeProvider theme={theme}>
       <AppContainer>
@@ -61,8 +52,8 @@ const App: FC = () => {
           <Route exact path="/add" component={AddItem} />
           <Route path="/item/:id" component={ItemDetails} />
         </Router>
-        <AdvertForm fields={fields} mutation={createAdvertisement}/>
-        <AdvertContainer/>
+        <AdvertForm fields={fields} mutation={createAdvertisement} />
+        <AdvertContainer />
       </AppContainer>
     </ThemeProvider>
   );
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,29 +1,42 @@
+/* eslint-disable react/destructuring-assignment */
+import QRCode from "qrcode.react";
 import React from "react";
 import useForm from "../hooks/useForm";
-import { IFields } from '../interfaces/IForm'
+import { IFields } from "../interfaces/IForm";
 
-export default function AdvertForm(props: { fields: IFields[], mutation: string }) {
-  const { values, handleInputChange, handleSubmit } = useForm({}, props.mutation);
+export default function AdvertForm(props: {
+  fields: IFields[];
+  mutation: string;
+}) {
+  const { values, handleInputChange, handleSubmit } = useForm(
+    {},
+    props.mutation
+  );
 
   const fields = props.fields.map((field, index) => {
     if (field.fieldType === "input") {
       return (
-        <input
-          key={index}
-          type={field.dataType}
-          name={field.name}
-          onChange={handleInputChange}
-          value={values[field.name]}
-        ></input>
+        <div>
+          <input
+            key={index}
+            type={field.dataType}
+            name={field.name}
+            onChange={handleInputChange}
+            value={values[field.name]}
+          />
+
+          <QRCode value={"Johanna"} level="L" />
+        </div>
       );
-    } else if (field.fieldType === "textarea") {
+    }
+    if (field.fieldType === "textarea") {
       return (
         <textarea
           key={index}
           name={field.name}
           onChange={handleInputChange}
           value={values[field.name]}
-        ></textarea>
+        />
       );
     }
   });

5ebcd2f0636609365fd7898e188e913041f53d09
Eric Rosenborg <eric.rosenborg@hiq.se>
2020-10-14 14:57:06 +0200

    Format

--- src/components/QRCodeContainer.tsx
+++ src/components/QRCodeContainer.tsx
@@ -1,29 +1,28 @@
-import API, { GraphQLResult } from '@aws-amplify/api';
-import { graphqlOperation } from 'aws-amplify';
-import React, { useEffect, useState } from 'react'
-import { GetAdvertisementQuery } from '../API';
-import { getAdvertisement } from '../graphql/queries';
-import QRCode from 'qrcode.react'
+import API, { GraphQLResult } from "@aws-amplify/api";
+import { graphqlOperation } from "aws-amplify";
+import React, { useEffect, useState } from "react";
+import { GetAdvertisementQuery } from "../API";
+import { getAdvertisement } from "../graphql/queries";
+import QRCode from "qrcode.react";
 
-export default function QRCodeContainer(props:{id: string}) {
+export default function QRCodeContainer(props: { id: string }) {
+  const [item, setItem] = useState({}) as any;
 
-    const [item, setItem] = useState({}) as any;
+  const fetchItems = async () => {
+    const result = (await API.graphql(
+      graphqlOperation(getAdvertisement, { id: props.id })
+    )) as GraphQLResult<GetAdvertisementQuery>;
+    const qrValue = JSON.stringify(result.data?.getAdvertisement);
+    setItem(qrValue);
+  };
 
-    const fetchItems = async () => {
-        const result = (await API.graphql(
-          graphqlOperation(getAdvertisement, {id: props.id})
-        )) as GraphQLResult<GetAdvertisementQuery>;
-        const qrValue = JSON.stringify(result.data?.getAdvertisement);
-        setItem(qrValue);
-    };
-    
-    useEffect(() => {
-        fetchItems();
-    }, []);
+  useEffect(() => {
+    fetchItems();
+  }, []);
 
-    return (
-        <div>
-            <QRCode value={item}></QRCode>
-        </div>
-    )
+  return (
+    <div>
+      <QRCode value={item}></QRCode>
+    </div>
+  );
 }

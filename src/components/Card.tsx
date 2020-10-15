d5a8cc30c1e94b25c24c520d31d9c5acf0dee87a
Anna Eklöv <annaeklov90@gmail.com>
2020-10-15 15:13:00 +0200

    cards have color and are clickable, writes in console

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,21 +1,33 @@
 import React, { FC } from "react";
+import styled from "styled-components";
 
 interface Props {
   title: string;
   description: string;
   width: number;
-  id: string
+  id: string;
 }
+
+const CardDiv = styled.div`
+  width: 100%;
+  background-color: #fbf7f0;
+  margin-top: 10px;
+  border-radius: 2px;
+  box-sizing: border-box;
+  :active {
+    opacity: 0.8;
+  }
+`;
 
 const Card = (props: Props) => {
   return (
-    <div id={props.id}>
+    <CardDiv id={props.id} onClick={() => console.log(props.id)}>
       <h2>{props.title}</h2>
 
       <p>{props.description}</p>
 
       <p>{props.width}</p>
-    </div>
+    </CardDiv>
   );
 };
 

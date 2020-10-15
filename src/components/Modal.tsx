195f9ae7e0089f29823504d6a24d28ecdba80eca
Anna Eklöv <annaeklov90@gmail.com>
2020-10-15 11:48:37 +0200

    made modal fit screen

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -3,15 +3,15 @@ import { createPortal } from "react-dom";
 import styled from "styled-components";
 
 const ModalDiv = styled.div`
-  position: absolute;
+  position: fixed;
   left: 50%;
   top: 50%;
   transform: translate(-50%, -50%);
   display: flex;
   align-items: center;
   justify-content: center;
-  width: 50%;
-  height: 50%;
+  width: 100vw;
+  height: 100vh;
   background-color: #d35098;
 `;
 

a4e7716e77c71a52134429a6dd48216fcfac2231
Johanna <nijodotcom@gmail.com>
2020-10-15 09:27:36 +0200

    cleanup code

--- src/components/OpenCamera.tsx
+++ src/components/OpenCamera.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useState } from "react";
+import React, { FC } from "react";
 import QrReader from "react-qr-reader";
 
 interface ITest {
@@ -17,8 +17,6 @@ interface Props {
 }
 
 const OpenCamera: FC<Props> = ({ test, setTest }) => {
-  console.log("VAD ÄR DU?    ", test);
-
   const handleScan = (result: any): any => {
     if (result) {
       console.log(result);
@@ -32,28 +30,13 @@ const OpenCamera: FC<Props> = ({ test, setTest }) => {
 
   return (
     <div>
-      <button onClick={() => setTest({ delay: 500, result: "" })}>Click</button>
-      <button onClick={() => setTest({ ...test, result: "New res" })}>
-        Click
-      </button>
-      <button
-        onClick={() =>
-          setTest((prev) => {
-            return { ...prev, result: "New res again" };
-          })
-        }
-      >
-        Click
-      </button>
       <QrReader
         delay={300}
         onError={handleError}
         onScan={handleScan}
-        style={{ width: "50%" }}
+        style={{ width: "100%" }}
       />
-      <p>
-        {test.result} and {test.delay}
-      </p>
+      <p>{test.result}</p>
     </div>
   );
 };

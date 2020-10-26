6f0d6f62ca4d80ef73dfd6bf709ad49cdd6972b7
Eric Rosenborg <eric.rosenborg@hiq.se>
2020-10-26 20:02:38 +0100

    Merge with develop and fix conflicts

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,14 +7,29 @@ import ModalAddItemContent from "./ModalAddItemContent";
 import { AmplifySignOut } from "@aws-amplify/ui-react";
 
 const HeaderDiv = styled.header`
-  width: 100%;
-  display: flex;
-  align-items: center;
-  justify-content: space-around;
-  background-color: #fbf7f0;
+  width: ${(props) => `${props.theme.headerTheme.width}%`};
+  height: ${(props) => `${props.theme.headerTheme.height}px`};
+  display: ${(props) => props.theme.headerTheme.display};
+  flex-direction: ${(props) => props.theme.headerTheme.flexDirection};
+  align-items: ${(props) => props.theme.headerTheme.alignItems};
+  justify-content: ${(props) => props.theme.headerTheme.justifyContent};
+  padding: ${(props) =>
+    `${props.theme.headerTheme.padding[0]}px ${props.theme.headerTheme.padding[1]}px ${props.theme.headerTheme.padding[2]}px ${props.theme.headerTheme.padding[3]}px`};
+  background-color: ${(props) => props.theme.headerTheme.backgroundColor};
+  box-sizing: border-box;
 
-  button {
-    cursor: pointer;
+  h2 {
+    font-style: ${(props) => props.theme.headerTheme.fontStyle};
+    font-weight: ${(props) => props.theme.headerTheme.fontWeight};
+    font-size: ${(props) => `${props.theme.headerTheme.fontSize}px`};
+    line-height: ${(props) => `${props.theme.headerTheme.lineHeight}%`};
+  }
+
+  a {
+    text-decoration: none;
+  }
+  a:visited {
+    color: black;
   }
 
   a {
@@ -42,7 +57,10 @@ const Header: FC<Props> = ({ modalOpen, setModalOpen, setAlreadyAQRCode }) => {
       </Modal>
 
       <Link to="/">
-        <h1>Haffa</h1>
+        <h2>
+          Sånt du kan <br />
+          Haffa!
+        </h2>
       </Link>
       <button
         onClick={() => {

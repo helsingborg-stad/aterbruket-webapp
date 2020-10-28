c1a6fe6905c7e5d37fe95e1e2c7be546f792c4e0
Anna Eklöv <annaeklov90@gmail.com>
2020-10-28 11:03:46 +0100

    merge wip into feature

---
+++ src/components/CarouselComp.tsx
@@ -0,0 +1,83 @@
+import React, { FC } from "react";
+import styled from "styled-components";
+import Carousel from "styled-components-carousel";
+
+const CarouselWrapper = styled.div`
+  width: 100%;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: white;
+
+  .closeBtn {
+    position: absolute;
+    top: 0;
+    right: 0;
+    z-index: 3;
+  }
+  //genererat classnam
+  .bOCFav {
+    height: 100vh;
+  }
+  //genererat classnam
+  .sc-pNWxx {
+    display: flex;
+  }
+  .imgDiv {
+    align-self: center;
+  }
+  img {
+    object-fit: cover;
+    width: 100%;
+    height: 100%;
+    align-self: stretch;
+  }
+`;
+
+type Props = {
+  setShowCarousel: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+const CarouselComp: FC<Props> = ({ setShowCarousel }: Props) => {
+  return (
+    <CarouselWrapper>
+      <button
+        className="closeBtn"
+        type="button"
+        onClick={() => setShowCarousel(false)}
+      >
+        X
+      </button>
+
+      <Carousel
+        center
+        infinite
+        swipeable
+        showArrows
+        showIndicator
+        slidesToShow={1}
+      >
+        <div className="imgDiv">
+          <img
+            src="https://storage.googleapis.com/web-pro-nilo-kavehome/media/cache/c4/10/c410118add2b5cb169d71a0c20596f50.jpg"
+            alt=""
+          />
+        </div>
+        <div className="imgDiv">
+          <img
+            src="https://i.pinimg.com/originals/10/c3/98/10c3987a6e929335135bf4dae89ac212.jpg"
+            alt=""
+          />
+        </div>
+        <div className="imgDiv">
+          <img
+            src="https://www.officestock.com.au/assets/full/cdkpedestal.jpg?20181003095800"
+            alt=""
+          />
+        </div>
+      </Carousel>
+    </CarouselWrapper>
+  );
+};
+export default CarouselComp;

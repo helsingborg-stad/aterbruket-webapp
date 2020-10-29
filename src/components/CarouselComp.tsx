cb0e3afffa3843d4a7a2005d5bd2389acfe8376a
Anna Eklöv <annaeklov90@gmail.com>
2020-10-29 11:52:00 +0100

    small change on carousel

--- src/components/CarouselComp.tsx
+++ src/components/CarouselComp.tsx
@@ -4,16 +4,16 @@ import Carousel from "styled-components-carousel";
 
 const CarouselWrapper = styled.div`
   width: 100%;
+  height: 100vh;
   position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
+  top: 0;
   background-color: white;
+  display: flex;
 
   .closeBtn {
     position: absolute;
-    top: 0;
-    right: 0;
+    top: 20px;
+    right: 10px;
     z-index: 3;
   }
   //genererat classnam

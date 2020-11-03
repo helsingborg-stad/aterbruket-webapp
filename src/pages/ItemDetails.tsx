b8a1c04d38d3c36b0cc36147e305e64566733a89
Johanna <nijodotcom@gmail.com>
2020-11-03 10:22:36 +0100

    merged with develop

--- src/pages/ItemDetails.tsx
+++ src/pages/ItemDetails.tsx
@@ -179,52 +179,52 @@ const ItemDetails: FC<ParamTypes> = () => {
       <Table>
         <tbody>
           <tr>
-            <td>Category:</td>
-            <td>Table</td>
+            <td>Kategori/Typ av möbel:</td>
+            <td>{item.category}</td>
           </tr>
           <tr>
             <td>Id:</td>
             <td>{id}</td>
           </tr>
           <tr>
-            <td>Height(cm):</td>
-            <td>200</td>
+            <td>Höjd:</td>
+            <td>{item.height} cm</td>
           </tr>
           <tr>
-            <td>Width (cm):</td>
-            <td>{item.width}</td>
+            <td>Bredd:</td>
+            <td>{item.width} cm</td>
           </tr>
           <tr>
-            <td>Depth (cm):</td>
-            <td>50</td>
+            <td>Djup:</td>
+            <td>{item.length} cm</td>
           </tr>
           <tr>
-            <td>Color:</td>
-            <td>Grey</td>
+            <td>Färg:</td>
+            <td>{item.color}</td>
           </tr>
 
           <tr>
             <td>Material:</td>
-            <td>Wood</td>
+            <td>{item.material}</td>
           </tr>
           <tr>
-            <td>Item Condition:</td>
-            <td>Good</td>
+            <td>Skick:</td>
+            <td>{item.condition}</td>
           </tr>
           <tr>
-            <td>Areas of Usage:</td>
-            <td>Indoor office</td>
+            <td>Användningsområde:</td>
+            <td>{item.areaOfUse}</td>
           </tr>
           <tr>
-            <td>LCA Value:</td>
+            <td>Klimatpåverkan:</td>
             <td>50</td>
           </tr>
           <tr>
-            <td>Description:</td>
+            <td>Beskrivning:</td>
             <td>{item.description}</td>
           </tr>
           <tr>
-            <td>Location:</td>
+            <td>Hämtas på:</td>
             <td>{item.location}</td>
           </tr>
         </tbody>

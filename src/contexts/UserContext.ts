7a1d649ed20cf9c430cab277850fdddcf7f579a1
Eric Rosenborg <eric.rosenborg@hiq.se>
2020-11-15 14:44:22 +0100

    Merge

--- src/contexts/UserContext.ts
+++ src/contexts/UserContext.ts
@@ -1,4 +1,4 @@
-import { Auth } from 'aws-amplify';
+import { API, Auth } from 'aws-amplify';
 import { createContext } from 'react';
 
 export const UserContext = createContext({attributes:{sub:'test'}});

1c3366cb9d68f4f86124a1ab4904a93f285620bd
Anna Eklöv <annaeklov90@gmail.com>
2020-11-19 18:28:31 +0100

    able to download div as pdf

--- src/components/QRCodeContainer.tsx
+++ src/components/QRCodeContainer.tsx
@@ -1,19 +1,21 @@
 import React, { FC } from "react";
 import QRCode from "qrcode.react";
 import styled from "styled-components";
+import html2canvas from "html2canvas";
+import { jsPDF } from "jspdf";
 import HBGLogo from "../pics/HBG_logo_staende_SV.jpg";
 
 const QRCodeCont = styled.div`
   display: flex;
-  margin-bottom: 5em;
+  margin-bottom: 1em;
   margin-top: 25px;
   align-items: center;
   flex-direction: column;
-  .labelDiv {
+  #labelDiv {
     border: 1px solid black;
     width: 265px;
     height: 121px;
-     border-radius: 9.5px;
+    border-radius: 9.5px;
     box-shadow: 0px 0px 2px rgba(98, 98, 98, 0.18),
       0px 1px 2px rgba(98, 98, 98, 0.18);
   }
@@ -28,7 +30,6 @@ const QRCodeCont = styled.div`
     display: flex;
     justify-content: space-between;
     padding: 7px 16px 7px 16px;
-   
   }
   .pDownload {
     color: grey;
@@ -47,8 +48,7 @@ interface IProps {
   id: string;
 }
 
-const QRCodeContainer: FC<IProps> = ({ id }: IProps) => {
-  const downloadQRCode = () => {
+/* const downloadQRCode = () => {
     const canvas: HTMLElement | null = document.getElementById("mycanvas");
     if (canvas !== null) {
       const image = (canvas as HTMLCanvasElement).toDataURL("image/png");
@@ -59,10 +59,33 @@ const QRCodeContainer: FC<IProps> = ({ id }: IProps) => {
       downloadLink.click();
       document.body.removeChild(downloadLink);
     }
+  }; */
+
+const QRCodeContainer: FC<IProps> = ({ id }: IProps) => {
+  const downloadLabel = () => {
+    const doc = new jsPDF("l", "px", "a4", true);
+
+    const pdfDiv: any | null = document.getElementById("labelDiv");
+    console.log(doc);
+
+    if (pdfDiv !== null) {
+      doc.html(pdfDiv, {
+        callback: function (doc) {
+          doc.save("sample-file.pdf");
+        },
+      });
+    }
   };
   return (
     <QRCodeCont>
-      <div className="labelDiv">
+      <button type="button" id="downloadBtn" onClick={downloadLabel}>
+        Spara som PDF
+      </button>
+      <div
+        className="labelDiv"
+        id="labelDiv"
+        style={{ width: "265px", height: "121px", margin: "10px" }}
+      >
         <p className="pTop">
           DEN HÄR KAN DU <strong>HAFFA</strong>
         </p>
@@ -71,8 +94,14 @@ const QRCodeContainer: FC<IProps> = ({ id }: IProps) => {
         </p>
         <p>Scanna QR-koden och bidra till en mer hållbar värld.</p>
         <div className="qrAndLogoDiv">
-          <QRCode value={id} id="mycanvas" size={50} onClick={downloadQRCode} />
-          <img src={HBGLogo} alt="Helsingborg logga" />
+          <QRCode value={id} id="mycanvas" size={50} />
+          <img
+            src={HBGLogo}
+            alt="Helsingborg logga"
+            width="46"
+            height="48"
+            style={{ paddingLeft: "130px" }}
+          />
         </div>
       </div>
       <p className="pDownload">

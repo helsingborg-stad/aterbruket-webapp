85751702c0559863eb7a7d06a85ca17db6b31ea0
Jonatan Hanson <jonatan.hanson@gmail.com>
2021-04-12 15:30:30 +0200

    Handle saving onboarding visibility to local storage

--- src/pages/StartScreen.tsx
+++ src/pages/StartScreen.tsx
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { FC, useEffect, useState } from "react";
+import { Auth } from "aws-amplify";
+import { Redirect } from "react-router-dom";
 
-const StartScreen: React.FC = () => {
-  return <div>Start screen</div>;
+const StartScreen: FC = () => {
+  const [redirect, setRedirect] = useState<string | null>(null);
+
+  const showOnboardingScreen = localStorage.getItem('HaffaApp:showOnboardingScreen');
+
+  useEffect(() => {
+    const redirectUser = async () => {
+      await Auth.currentAuthenticatedUser()
+        .then(() => {
+          setRedirect('app');
+        })
+        .catch(() => {
+          setRedirect(showOnboardingScreen === 'false' ? 'app' : 'onboarding');
+        });
+    }
+
+    redirectUser();
+  }, []);
+
+  if (redirect) {
+    return <Redirect to={redirect} />
+  }
+
+  return <div>Loading...</div>;
 };
 
 export default StartScreen;

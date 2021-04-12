b3369508303edbcd30799ab98343c940fd4669d2
Jonatan Hanson <jonatan.hanson@gmail.com>
2021-04-12 13:37:53 +0200

    Moved auth logic from App component and made some clean up

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
 /* eslint-disable import/no-named-as-default-member */
-import React, { FC, useContext, useEffect, useState } from "react";
+import React, { FC, useState } from "react";
 import { Route, BrowserRouter as Router } from "react-router-dom";
 import styled, { ThemeProvider } from "styled-components";
-import { withAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react";
-import { Auth, Storage } from "aws-amplify";
+import { withAuthenticator } from "@aws-amplify/ui-react";
 import "./App.css";
 import theme from "./styles/theme";
 import Home from "./pages/Home";
@@ -12,12 +11,13 @@ import AddItem from "./pages/AddItem";
 import ItemDetails from "./pages/ItemDetails";
 import Profile from "./pages/Profile";
 import Haffat from "./pages/Haffat";
+import Onboarding from "./pages/Onboarding";
+import StartScreen from "./pages/StartScreen";
 import MenuBar from "./components/MenuBar";
-import { UserContext } from "./contexts/UserContext";
+import { UserProvider } from "./contexts/UserContext";
 import PersonalInfo from "./components/PersonalInfo";
 import Statics from "./components/Statics";
 import MyAdverts from "./components/MyAdverts";
-import { v4 as uuidv4 } from "uuid";
 import ReactPWAInstallProvider, { useReactPWAInstall } from "react-pwa-install";
 
 const AppContainer = styled.div`
@@ -45,14 +45,7 @@ const App: FC = () => {
   const [modalOpen, setModalOpen] = useState(false);
   const [alreadyAQRCode, setAlreadyAQRCode] = useState(false);
   const [qrCamera, setQrCamera] = useState({ delay: 500, result: "" });
-  const [user, setUser] = useState({ attributes: {} }) as any;
   const { pwaInstall, supported, isInstalled } = useReactPWAInstall();
-
-  useEffect(() => {
-    Auth.currentAuthenticatedUser().then((user) => {
-      setUser(user);
-    });
-  }, []);
 
   const handleClick = () => {
     pwaInstall({
@@ -62,9 +55,8 @@ const App: FC = () => {
       .then()
       .catch(() => alert("Ladda gärna ner den nästa gång du använder Haffa."));
   };
-
   return (
-    <UserContext.Provider value={user}>
+    <UserProvider>
       <ThemeProvider theme={theme}>
         <AppContainer>
           {supported() && !isInstalled() && (
@@ -74,42 +66,42 @@ const App: FC = () => {
           )}
           <Router>
             <Header isInDetail={false} />
-            <UserContext.Provider value={user}>
-              <Route
-                exact
-                path="/"
-                component={() => (
-                  <Home
-                    modalOpen={modalOpen}
-                    setModalOpen={setModalOpen}
-                    setAlreadyAQRCode={setAlreadyAQRCode}
-                    qrCamera={qrCamera}
-                    setQrCamera={setQrCamera}
-                  />
-                )}
-              />
-              <Route
-                path="/add"
-                component={() => (
-                  <AddItem
-                    alreadyAQRCode={alreadyAQRCode}
-                    qrCamera={qrCamera}
-                    setQrCamera={setQrCamera}
-                  />
-                )}
-              />
-              <Route path="/haffat" component={Haffat} />
-              <Route exact path="/profile" component={Profile} />
-              <Route path="/profile/personal-info" component={PersonalInfo} />
-              <Route path="/profile/statics" component={Statics} />
-              <Route path="/profile/myadverts" component={MyAdverts} />
-              <Route path="/item/:id" component={ItemDetails} />
-            </UserContext.Provider>
+            <Route exact path="/" component={StartScreen} />
+            <Route exact path="/onboarding" component={Onboarding} />
+            <Route
+              exact
+              path="/app"
+              component={() => (
+                <Home
+                  modalOpen={modalOpen}
+                  setModalOpen={setModalOpen}
+                  setAlreadyAQRCode={setAlreadyAQRCode}
+                  qrCamera={qrCamera}
+                  setQrCamera={setQrCamera}
+                />
+              )}
+            />
+            <Route
+              path="/add"
+              component={() => (
+                <AddItem
+                  alreadyAQRCode={alreadyAQRCode}
+                  qrCamera={qrCamera}
+                  setQrCamera={setQrCamera}
+                />
+              )}
+            />
+            <Route path="/haffat" component={Haffat} />
+            <Route exact path="/profile" component={Profile} />
+            <Route path="/profile/personal-info" component={PersonalInfo} />
+            <Route path="/profile/statics" component={Statics} />
+            <Route path="/profile/myadverts" component={MyAdverts} />
+            <Route path="/item/:id" component={ItemDetails} />
             <MenuBar setQrCamera={setQrCamera} qrCamera={qrCamera} />
           </Router>
         </AppContainer>
       </ThemeProvider>
-    </UserContext.Provider>
+    </UserProvider>
   );
 };
 

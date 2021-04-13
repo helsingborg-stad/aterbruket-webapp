bcf7244b8972f940e77513c2c61cad0368454c1e
Jonatan Hanson <jonatan.hanson@gmail.com>
2021-04-13 15:42:27 +0200

    Created sign in window with zero design

---
+++ src/pages/SignIn.js
@@ -0,0 +1,48 @@
+
+import React, { FC, useEffect } from "react";
+import { AmplifySignIn, AmplifyForgotPassword as AwsForgotPassword } from "@aws-amplify/ui-react";
+import styled from "styled-components";
+import { withRouter } from 'react-router-dom'
+
+const AmplifySignInContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const LoginContainer = styled.div``;
+
+const SignIn = () => {
+
+  return (
+    <AmplifySignInContainer slot="sign-in">
+      <LoginContainer>
+        <h4>En delningsplattform</h4>
+        <h1>Haffa!</h1>
+        <h3>Logga in med ditt vanliga jobbkonto - ingen registrering behövs. </h3>
+      </LoginContainer>
+      <AmplifySignIn
+        headerText=""
+        submitButtonText="Logga in"
+        usernameAlias="username"
+        hideSignUp
+        formFields={[
+          {
+            type: "username",
+            label: "Användarnamn",
+            placeholder: "Ditt användarnamn",
+            required: true,
+          },
+          {
+            type: "password",
+            label: "Lösenord",
+            placeholder: "Fyll i ditt lösenord",
+            required: true,
+            hint: '' // <-- Hides forgotten password link
+          },
+        ]}>
+      </AmplifySignIn>
+    </AmplifySignInContainer>
+  );
+}
+
+export default withRouter(SignIn);

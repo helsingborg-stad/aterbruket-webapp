9346194accce7e4b3ca41b8bbd6a5419a9c842a3
Jonatan Hanson <jonatan.hanson@gmail.com>
2021-04-13 15:38:10 +0200

    Moved app specific styling to app router

--- src/App.tsx
+++ src/App.tsx
@@ -6,27 +6,6 @@ import RootRouter from './routes/RootRouter';
 import theme from "./styles/theme";
 import ReactPWAInstallProvider, { useReactPWAInstall } from "react-pwa-install";
 
-const AppContainer = styled.div`
-  min-height: ${(props) => `${props.theme.appTheme.minHeight}vh`};
-  min-width: ${(props) => `${props.theme.appTheme.minWidth}px`};
-  width: ${(props) => `${props.theme.appTheme.width}%`};
-  padding: ${(props) =>
-    `${props.theme.appTheme.padding[0]}rem ${props.theme.appTheme.padding[1]}rem ${props.theme.appTheme.padding[2]}rem ${props.theme.appTheme.padding[3]}rem`};
-  box-sizing: ${(props) => props.theme.appTheme.boxSizing};
-  background-color: ${(props) => props.theme.appTheme.primaryColor};
-  font-family: ${(props) => props.theme.appTheme.fontFamily};
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  main {
-    width: 100%;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    margin-bottom: 65px;
-  }
-`;
-
 const App: FC = () => {
   const { pwaInstall, supported, isInstalled } = useReactPWAInstall();
   const handleClick = () => {
@@ -40,14 +19,12 @@ const App: FC = () => {
   return (
     <UserProvider>
       <ThemeProvider theme={theme}>
-        <AppContainer>
-          <RootRouter />
+        <RootRouter />
           {supported() && !isInstalled() && (
             <button type="button" onClick={handleClick}>
               Lägg Haffa på hemskärmen
             </button>
           )}
-        </AppContainer>
       </ThemeProvider>
     </UserProvider>
   );

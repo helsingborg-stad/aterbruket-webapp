dcab4287034df66065a8241373b4e72d73f63967
Jonatan Hanson <jonatan.hanson@gmail.com>
2021-04-15 11:32:46 +0200

    Reqrote user context with User types

--- src/contexts/UserContext.tsx
+++ src/contexts/UserContext.tsx
@@ -1,24 +1,49 @@
-import { Auth } from "aws-amplify";
 import React, { useState, useEffect } from "react";
+import { onAuthUIStateChange } from '@aws-amplify/ui-components';
 
-const UserContext = React.createContext({});
+export interface User {
+  name?: string,
+  sub?: string,
+  email?: string,
+  department?: string,
+  address?: string,
+  postalcode?: string,
+}
+
+export interface UserContextType {
+  user: User,
+  authState: string
+}
 
 interface ProviderProps {
   children: React.ReactNode;
 }
 
-function UserProvider({ children }: ProviderProps) {
-  const [user, setUser] = useState({ attributes: {} });
+const UserContext = React.createContext<UserContextType>(
+  { user: {}, authState: '' }
+);
 
-  useEffect(() => {
-    Auth.currentAuthenticatedUser()
-      .then((user) => {
-        setUser(user);
-      })
-      .catch((err) => console.log("Failed fetching current user: ", err));
+function UserProvider({ children }: ProviderProps) {
+  const [authState, setAuthState] = useState<string>('');
+  const [user, setUser] = useState<User>({});
+
+  React.useEffect(() => {
+    return onAuthUIStateChange((nextAuthState, authData: any) => {
+      setAuthState(nextAuthState);
+      if (authData?.attributes) {
+        setUser({
+          name: authData?.attributes?.name || '',
+          sub: authData?.attributes?.sub || '',
+          email: authData?.attributes?.email || '',
+          department: authData?.attributes["custom:department"] || '',
+          address: authData?.attributes?.address || '',
+          postalcode: authData?.attributes["custom:postalcode"] || '',
+        });
+      }
+    });
   }, []);
 
-  return <UserContext.Provider value={user}>{children}</UserContext.Provider>;
+  return <UserContext.Provider value={{ user, authState }}>{children}</UserContext.Provider>;
 }
 
 export { UserProvider };

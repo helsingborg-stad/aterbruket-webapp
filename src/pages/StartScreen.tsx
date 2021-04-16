af7663c126865dcbf2cf14d62817f52a5638badf
Jonatan Hanson <jonatan.hanson@gmail.com>
2021-04-16 14:40:34 +0200

    Added loading spinner on start screen setup

--- src/pages/StartScreen.tsx
+++ src/pages/StartScreen.tsx
@@ -1,9 +1,18 @@
-import React, { FC, useEffect, useState } from "react";
+import React, { FC, useEffect } from "react";
 import { Auth } from "aws-amplify";
-import { Redirect } from "react-router-dom";
+import { RouteComponentProps } from "react-router-dom";
+import Loader from "react-loader-spinner";
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+import styled from "styled-components";
 
-const StartScreen: FC = () => {
-  const [redirect, setRedirect] = useState<string | null>(null);
+const Container = styled.div`
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const StartScreen: FC<RouteComponentProps> = ({ history }) => {
   const showOnboardingScreen = localStorage.getItem(
     "HaffaApp:showOnboardingScreen"
   );
@@ -12,21 +21,21 @@ const StartScreen: FC = () => {
     const redirectUser = async () => {
       await Auth.currentAuthenticatedUser()
         .then(() => {
-          setRedirect("app");
+          history.push("app");
         })
         .catch(() => {
-          setRedirect(showOnboardingScreen === "false" ? "app" : "onboarding");
+          history.push(showOnboardingScreen === "false" ? "app" : "onboarding");
         });
     };
-
     redirectUser();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  if (redirect) {
-    return <Redirect to={redirect} />;
-  }
-
-  return <div>Loading...</div>;
+  return (
+    <Container>
+      <Loader type="Rings" color="#50811b" height={80} width={80} />
+    </Container>
+  );
 };
 
 export default StartScreen;

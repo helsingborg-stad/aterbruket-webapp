ebb5646407e01c5d295ffac7873c9634489a2b3c
Rubi Kuo <rubitiara@gmail.com>
2021-04-21 15:43:00 +0200

    Revert "adjusted CSS"
    This reverts commit 0aff563f0448290b9784d0752b6d5efc3cd13499.

--- src/pages/ItemDetails.tsx
+++ src/pages/ItemDetails.tsx
@@ -201,8 +201,7 @@ const MainSection = styled.section`
     color: ${(props) => props.theme.colors.primary};
   }
   .dark {
-    text-align: left;
-    margin: 48px 0 28px 16px;
+    margin: 48px 0 28px 0;
     color: ${(props) => props.theme.colors.darkest};
   }
 
@@ -212,6 +211,10 @@ const MainSection = styled.section`
     font-size: 16px;
     line-height: 150%;
     color: ${(props) => props.theme.colors.darkest};
+  }
+
+  div {
+    padding: 0 24px 0 24px;
   }
 
   table {
@@ -243,16 +246,14 @@ const MainSection = styled.section`
 const CardGroups = styled.div`
   display: flex;
   flex-direction: column;
-  width: 100%;
-
+  align-items: center;
   .card {
     display: flex;
     flex-direction: column;
     align-items: center;
-    margin: 0 16px;
-
-    min-width: 350px;
-    height: auto;
+    padding: 24px;
+    width: 382px;
+    height: 326px;
     background-color: ${(props) => props.theme.colors.white};
     border-radius: 0px 0px 9.5px 9.5px;
     filter: drop-shadow(0px 0px 2px rgba(98, 98, 98, 0.18)),
@@ -261,16 +262,14 @@ const CardGroups = styled.div`
 
   .cardHeader {
     width: 100%;
-    height: 128px;
+    height: 30%;
     display: flex;
     justify-content: center;
     align-items: center;
   }
   .cardBody {
-    box-sizing: border-box;
-    margin: 0 12px;
     width: 100%;
-    height: 198px;
+    height: 70%;
   }
   h5 {
     font-weight: 900;
@@ -312,7 +311,6 @@ const CardGroups = styled.div`
 
     h4 {
       margin: 0 16px;
-      text-align: left;
     }
     div {
       padding: 0;
@@ -332,9 +330,10 @@ const CardGroups = styled.div`
     }
   }
   .contactInfo {
+    padding: 0 8px 0 8px;
     display: flex;
     align-items: center;
-    padding-left: 15px;
+    width: 100%;
     height: 48px;
     background-color: #f5f5f5;
     border-radius: 4.5px;
@@ -519,6 +518,8 @@ const ItemDetails: FC<ParamTypes> = () => {
           </ImgDiv>
         )}
         <div className="titleDiv">
+          {/*           <h4>Möbler</h4>
+           */}
           <h1>{item.title}</h1>
         </div>
 
@@ -713,25 +714,41 @@ const ItemDetails: FC<ParamTypes> = () => {
                 <MdPlace />
               </Button>
             </div>
+            {/* <MapContainer>
+              {item && item.location && (
+                <Map
+                  mapType={google.maps.MapTypeId.ROADMAP}
+                  mapTypeControl={false}
+                  location={item.location}
+                />
+              )}
+
+              {!item.location && (
+                <Loader
+                  type="ThreeDots"
+                  color="#9db0c6"
+                  height={50}
+                  width={50}
+                />
+              )}
+            </MapContainer> */}
           </div>
           <h4 className="dark">Kontaktperson</h4>
 
           <div className="card">
-            <div className="cardBody">
-              <div className="contactPersonDiv">
-                <div>
-                  <MdPerson />
-                </div>
-                <h4 className="dark">{item.contactPerson}</h4>
+            <div className="contactPersonDiv">
+              <div>
+                <MdPerson />
               </div>
-              <div className="contactInfo">
-                <MdPhone />
-                <p>{item.phoneNumber}</p>
-              </div>
-              <div className="contactInfo">
-                <FiAtSign />
-                <p>{item.email}</p>
-              </div>
+              <h4 className="dark">{item.contactPerson}</h4>
+            </div>
+            <div className="contactInfo">
+              <MdPhone />
+              <p>{item.phoneNumber}</p>
+            </div>
+            <div className="contactInfo">
+              <FiAtSign />
+              <p>{item.email}</p>
             </div>
           </div>
         </CardGroups>
